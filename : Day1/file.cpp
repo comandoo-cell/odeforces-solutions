b3293c1ec4a38b2658acc206bb99260c74f09446
MUHAMMED MUHAMMED <lapmsi454@gmail.com>
2026-06-02 21:43:35 +0300

    Update file.cpp

--- : Day1/file.cpp
+++ : Day1/file.cpp
@@ -1,1 +1,24 @@
 
+#include<iostream>
+using namespace std;
+int main() {
+
+    int n,Min=1e9,A;
+    cin >> n;
+    while (n--) {
+        cin >> A;
+        if (A == 0) {
+            cout << 0;
+            break;
+        }
+        A = abs(A);
+        if (Min > A) {
+            Min = A;
+        }
+    }
+
+    cout << Min;
+
+
+    return 0;
+}
